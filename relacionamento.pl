4ccf373968331c3043f68afd8c9fd4f75a9153a2
anapaulavargas <anapaulavnoronha@gmail.com>
2013-10-20 19:21:44 -0200

    atributos possiveis para os FATOS

---
+++ relacionamento.pl
@@ -0,0 +1,8 @@
+pessoa(nome,sexo,idade,profissao,cidade,bairro,generoFilmePreferido,
+estiloMusicalPreferido, temFilhos).
+
+/* seria o inicio do nosso template pra fazer uma aplicacao
+como se fosse um site de relacionamento. Coloquei alguns atributos
+mas podem ter mais. A gente teria varias regras, por exemplo a idade,
+a proximidade do bairro das pessoas e etc. Além disso teria a prioridade
+determinado atributo é mais importante que outros, ideias? */
