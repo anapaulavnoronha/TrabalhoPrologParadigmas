811d56a69d1cd81da056623badf487f08e6d6129
anapaulavargas <anapaulavnoronha@gmail.com>
2013-10-22 00:49:09 -0200

    fatos

--- relacionamento.pl
+++ relacionamento.pl
@@ -1,6 +1,14 @@
 pessoa(nome,sexo,idade,profissao,cidade,bairro,generoFilmePreferido,
 estiloMusicalPreferido, temFilhos).
 
+pessoa(Fernanda, f, 24, jornalista, Brasília, Asa Sul, terror, samba, nao).
+pessoa(Maria, f, 46, advogada, Brasília, Sudoeste, comedia, rock, sim).
+pessoa(Eduardo, m, 32, professor, Brasília, Lago Norte, terror, jazz, nao).
+pessoa(Pedro, m, 21, jornalista, Rio de Janeiro, Ipanema, drama, rock, nao).
+pessoa(Camila, f, 34, engenheira civil, Brasília, Asa Sul, comedia, pop, sim).
+pessoa(Felipe, m, 51, fotografo, Brasília, Lago Sul, comedia, axe, sim).
+ 
+
 /* seria o inicio do nosso template pra fazer uma aplicacao
 como se fosse um site de relacionamento. Coloquei alguns atributos
 mas podem ter mais. A gente teria varias regras, por exemplo a idade,
