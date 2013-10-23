af44214c5e63827090a2feaf85de502d77bf9b00
Matheus Oliveira <matheus.tristao@gmail.com>
2013-10-22 23:42:02 -0200

    Correcao de erros de espaços

--- relacionamento.pl
+++ relacionamento.pl
@@ -1,12 +1,12 @@
 pessoa(nome,sexo,idade,profissao,cidade,bairro,generoFilmePreferido,
 estiloMusicalPreferido, temFilhos).
 
-pessoa(Fernanda, f, 24, jornalista, Brasília, Asa Sul, terror, samba, nao).
-pessoa(Maria, f, 46, advogada, Brasília, Sudoeste, comedia, rock, sim).
-pessoa(Eduardo, m, 32, professor, Brasília, Lago Norte, terror, jazz, nao).
-pessoa(Pedro, m, 21, jornalista, Rio de Janeiro, Ipanema, drama, rock, nao).
-pessoa(Camila, f, 34, engenheira civil, Brasília, Asa Sul, comedia, pop, sim).
-pessoa(Felipe, m, 51, fotografo, Brasília, Lago Sul, comedia, axe, sim).
+pessoa(Fernanda, f, 24, jornalista, Brasilia, Asa_Sul, terror, samba, nao).
+pessoa(Maria, f, 46, advogado, Brasilia, Sudoeste, comedia, rock, sim).
+pessoa(Eduardo, m, 32, professor, Brasilia, Lago_Norte, terror, jazz, nao).
+pessoa(Pedro, m, 21, jornalista, Sao_Paulo, Morumbi, drama, rock, nao).
+pessoa(Camila, f, 34, engenheiro_civil, Brasilia, Asa_Sul, comedia, pop, sim).
+pessoa(Felipe, m, 51, fotografo, Brasilia, Lago_Sul, comedia, axe, sim).
  
 
 /* seria o inicio do nosso template pra fazer uma aplicacao
