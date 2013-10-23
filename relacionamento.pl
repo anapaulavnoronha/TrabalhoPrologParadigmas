901796bc41ee74e0b4895a6a4f16adf3f81f0484
Matheus Oliveira <matheus.tristao@gmail.com>
2013-10-23 00:07:33 -0200

    Fato endereco, pessoa recebendo cep

--- relacionamento.pl
+++ relacionamento.pl
@@ -1,13 +1,21 @@
-pessoa(nome,sexo,idade,profissao,cidade,bairro,generoFilmePreferido,
+pessoa(nome,sexo,idade,profissao,cep,generoFilmePreferido,
 estiloMusicalPreferido, temFilhos).
 
-pessoa(Fernanda, f, 24, jornalista, Brasilia, Asa_Sul, terror, samba, nao).
-pessoa(Maria, f, 46, advogado, Brasilia, Sudoeste, comedia, rock, sim).
-pessoa(Eduardo, m, 32, professor, Brasilia, Lago_Norte, terror, jazz, nao).
-pessoa(Pedro, m, 21, jornalista, Sao_Paulo, Morumbi, drama, rock, nao).
-pessoa(Camila, f, 34, engenheiro_civil, Brasilia, Asa_Sul, comedia, pop, sim).
-pessoa(Felipe, m, 51, fotografo, Brasilia, Lago_Sul, comedia, axe, sim).
+endereco(cep,estado,cidade,bairro).
+
+pessoa(Fernanda, f, 24, jornalista, 70000, terror, samba, nao).
+pessoa(Maria, f, 46, advogado, 70001, comedia, rock, sim).
+pessoa(Eduardo, m, 32, professor, 70002, terror, jazz, nao).
+pessoa(Pedro, m, 21, jornalista, 70003, drama, rock, nao).
+pessoa(Camila, f, 34, engenheiro_civil, 70000, comedia, pop, sim).
+pessoa(Felipe, m, 51, fotografo, Brasilia, 70005, comedia, axe, sim).
  
+endereco(70000,Distrito_Federal,Brasilia,Asa_Sul).
+endereco(70001,Distrito_Federal,Brasilia,Sudoeste).
+endereco(70002,Distrito_Federal,Brasilia,Lago_Norte).
+endereco(70003,Sao_Paulo,Sao_Paulo,Morumbi).
+endereco(70004,Distrito_Federal,Brasilia,Asa_Norte).
+endereco(70005,Distrito_Federal,Brasilia,Lago_Sul).
 
 /* seria o inicio do nosso template pra fazer uma aplicacao
 como se fosse um site de relacionamento. Coloquei alguns atributos
