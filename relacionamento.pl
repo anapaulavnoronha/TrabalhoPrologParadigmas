759fd357ff7a6320bc039a4d976635000eea5fc3
Matheus Oliveira <matheus.tristao@gmail.com>
2013-10-23 23:06:26 -0200

    Alteração do atributo Cep pra Cidade em pessoa

--- relacionamento.pl
+++ relacionamento.pl
@@ -4,27 +4,29 @@ EstiloMusicalPreferido, TemFilhos).
 endereco(Cep,Estado,Cidade,Bairro).*/
 
 
-pessoa(fernanda, f, hetero, 24, jornalista, 70000, terror, samba, nao).
-pessoa(maria, f, hetero, 46, advogado, 70001, comedia, rock, sim).
-pessoa(eduardo, m, homo, 32, professor, 70002, terror, jazz, nao).
-pessoa(pedro, m, homo, 21, jornalista, 70000, drama, rock, nao).
-pessoa(camila, f, hetero, 34, engenheiro_civil, 70000, comedia, pop, sim).
-pessoa(felipe, m, hetero, 51, fotografo, 70001, comedia, axe, sim).
+pessoa(fernanda, f, hetero, 24, jornalista, brasilia, terror, samba, nao).
+pessoa(maria, f, hetero, 46, advogado, salvador, comedia, rock, sim).
+pessoa(eduardo, m, homo, 32, professor, brasilia, terror, jazz, nao).
+pessoa(pedro, m, homo, 21, jornalista, salvador, drama, rock, nao).
+pessoa(camila, f, hetero, 34, engenheiro_civil, brasilia, comedia, pop, sim).
+pessoa(felipe, m, hetero, 51, fotografo, brasilia, comedia, axe, sim).
 
-
+/*
 endereco(70000,distrito_Federal,brasilia,asa_Sul).
 endereco(70001,distrito_Federal,brasilia,lago_Sul).
 endereco(70002,distrito_Federal,brasilia,lago_Norte).
 endereco(70000,distrito_Federal,brasilia,asa_Sul).
 endereco(70004,distrito_Federal,brasilia,asa_Norte).
 endereco(70001,distrito_Federal,brasilia,lago_Sul).
+*/
+
 
 ifThenElse(NomeX, NomeY, _) :- NomeX, !, NomeY.
 ifThenElse(_,_,OpcaoSexual) :- OpcaoSexual.
 
 
-casal(NomeX,NomeY):-pessoa(NomeX,SexoX,OpcaoSexual,IdadeX,ProfissaoX,CepX,GeneroFilmeX,EstiloMusicalX)
-	            ,pessoa(NomeY,SexoY,OpcaoSexual,IdadeY,ProfissaoY,CepY,GeneroFilmeY,EstiloMusicalY)
+casal(NomeX,NomeY):-pessoa(NomeX,SexoX,OpcaoSexual,IdadeX,ProfissaoX,CidadeX,GeneroFilmeX,EstiloMusicalX)
+	            ,pessoa(NomeY,SexoY,OpcaoSexual,IdadeY,ProfissaoY,CidadeY,GeneroFilmeY,EstiloMusicalY)
 		    ,NomeX \= NomeY, ifThenElse(OpcaoSexual = homo, SexoX = SexoY, SexoX\=SexoY).
 
 
