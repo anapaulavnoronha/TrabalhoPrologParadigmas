3e938641b5165fea6088906119ddec98631a626c
Alexcortes <alex.co.alves@gmail.com>
2013-10-23 22:06:20 -0200

    Inclusão de opcaoSexual

--- relacionamento.pl
+++ relacionamento.pl
@@ -1,16 +1,17 @@
 /*pessoa(Nome,Sexo,Idade,Profissao,Cep,GeneroFilmePreferido,
-EstiloMusicalPreferido, TemFilhos).
+EstiloMusicalPreferido, TemFilhos, opcaoSexual).
 
 endereco(Cep,Estado,Cidade,Bairro).*/
 
 
-pessoa(fernanda, f, 24, jornalista, 70000, terror, samba, nao).
-pessoa(maria, f, 46, advogado, 70001, comedia, rock, sim).
-pessoa(eduardo, m, 32, professor, 70002, terror, jazz, nao).
-pessoa(pedro, m, 21, jornalista, 70000, drama, rock, nao).
-pessoa(camila, f, 34, engenheiro_civil, 70000, comedia, pop, sim).
-pessoa(felipe, m, 51, fotografo, 70001, comedia, axe, sim).
- 
+pessoa(fernanda, f, 24, jornalista, 70000, terror, samba, nao, hetero).
+pessoa(maria, f, 46, advogado, 70001, comedia, rock, sim, hetero).
+pessoa(eduardo, m, 32, professor, 70002, terror, jazz, nao, homo).
+pessoa(pedro, m, 21, jornalista, 70000, drama, rock, nao, homo).
+pessoa(camila, f, 34, engenheiro_civil, 70000, comedia, pop, sim, hetero).
+pessoa(felipe, m, 51, fotografo, 70001, comedia, axe, sim, hetero).
+
+
 endereco(70000,distrito_Federal,brasilia,asa_Sul).
 endereco(70001,distrito_Federal,brasilia,lago_Sul).
 endereco(70002,distrito_Federal,brasilia,lago_Norte).
@@ -18,7 +19,8 @@ endereco(70000,distrito_Federal,brasilia,asa_Sul).
 endereco(70004,distrito_Federal,brasilia,asa_Norte).
 endereco(70001,distrito_Federal,brasilia,lago_Sul).
 
-casal(X,Y):-pessoa(X,A,B,C,D,E,F,G),pessoa(Y,L,M,N,D,P,Q,R).
+
+casal(X,Y):-pessoa(X,A,B,C,D,E,F,G,Z),pessoa(Y,L,M,N,D,P,Q,R,Z).
 
 
 /* seria o inicio do nosso template pra fazer uma aplicacao
