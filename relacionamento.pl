fbb4e2baaa9a7d65a86985e647a62fc995c6dd69
Alexcortes <alex.co.alves@gmail.com>
2013-10-24 18:04:14 -0200

    Correção de redundancias, criação do método de comparação de gostos

--- relacionamento.pl
+++ relacionamento.pl
@@ -1,40 +1,65 @@
 /*pessoa(Nome,Sexo,OpcaoSexual,Idade,Profissao,Cep,GeneroFilmePreferido,
-EstiloMusicalPreferido, TemFilhos).
+EstiloMusicalPreferido, TemFilhos, Gosto1, Gosto2, Gosto3, Gosto4).
 */
 
 
-pessoa(fernanda, f, hetero, 24, jornalista, brasilia, terror, samba).
-pessoa(maria, f, hetero, 46, advogado, salvador, comedia, rock).
-pessoa(eduardo, m, homo, 32, professor, brasilia, terror, jazz).
-pessoa(pedro, m, homo, 21, jornalista, salvador, drama, rock).
-pessoa(camila, f, hetero, 34, engenheiro_civil, brasilia, comedia, pop).
-pessoa(felipe, m, hetero, 51, fotografo, brasilia, comedia, axe).
+/*Definição de opçoes a serem escolhidas como gosto, cada usuário deve escolher as 4 favoritas:
+	-Esporte
+	-Cinema
+	-Seriados
+	-Festa
+	-Estudos
+	-Musica
+	-Dança
+	-Trabalho
+	-Culinaria
+*/
 
 
-/* Regra para orientação sexual */
 
-ifThenElse(X, Y, _) :- X, !, Y.
-ifThenElse(_,_,OpcaoSexual) :- OpcaoSexual.
+/*BASE DE DADOS*/
+pessoa(fernanda, f, hetero, 24, jornalista, brasilia, terror, samba, esporte, cinema, seriados, festa).
+pessoa(maria, f, hetero, 46, advogado, salvador, comedia, rock, cinema, danca, trabalho, culinaria).
+pessoa(eduardo, m, hetero, 25, professor, brasilia, terror, jazz, esporte, cinema, seriados, festa).
+pessoa(pedro, m, homo, 21, jornalista, salvador, drama, rock, seriados, danca, esporte, festa).
+pessoa(camila, f, hetero, 34, engenheiro_civil, brasilia, comedia, pop, trabalho, musica, estudo, cinema).
+pessoa(felipe, m, hetero, 51, fotografo, brasilia, comedia, axe, musica, seriado, festa, cinema).
 
 
-casal(NomeX,NomeY):-pessoa(NomeX,SexoX,OpcaoSexual,IdadeX,ProfissaoX,CidadeX,GeneroFilmeX,EstiloMusicalX)
-	            ,pessoa(NomeY,SexoY,OpcaoSexual,IdadeY,ProfissaoY,CidadeY,GeneroFilmeY,EstiloMusicalY)
-		    ,NomeX \= NomeY, ifThenElse(OpcaoSexual = homo, SexoX = SexoY, SexoX\=SexoY).
 
-/* Regra para cidade */
-casal(NomeX,NomeY):-pessoa(NomeX,SexoX,OpcaoSexual,IdadeX,ProfissaoX,CidadeX,GeneroFilmeX,EstiloMusicalX)
-	            ,pessoa(NomeY,SexoY,OpcaoSexual,IdadeY,ProfissaoY,CidadeX,GeneroFilmeY,EstiloMusicalY)
-		    ,NomeX \= NomeY, ifThenElse(OpcaoSexual = homo, SexoX = SexoY, SexoX\=SexoY).
 
+/* Criação  de uma regra IF THEN ELSE */
+
+ifThenElse(IF, THEN, _) :- IF, !, THEN.
+ifThenElse(_,_,ELSE) :- ELSE.
 
 
 /* Regra para idade */
 
 idade(IdadeX,IdadeY) :- (IdadeX-IdadeY)=<7 , (IdadeY-IdadeX)=<7.
 
-casal(NomeX,NomeY) :- pessoa(NomeX,SexoX,OpcaoSexualX,IdadeX,ProfissaoX,CidadeX,GeneroFilmeX,EstiloMusicalX),
-	              pessoa(NomeY,SexoY,OpcaoSexualY,IdadeY,ProfissaoY,CidadeY,GeneroFilmeY,EstiloMusicalY),
-		      idade(IdadeX,IdadeY).
+
+
+
+/*Regra para comparar gostos(Para as pessoas serem compativeis, todos os gostos devem ser iguais)*/
+
+gostos(NomeX, NomeY) :- pessoa(NomeX,_,_,_,_,_,_,_,Gosto1,Gosto2,Gosto3,Gosto4), pessoa(NomeY,_,_,_,_,_,_,_,Gosto5,Gosto6,Gosto7,Gosto8), 
+			(Gosto1 = Gosto5; Gosto1 = Gosto6; Gosto1 = Gosto7; Gosto1 = Gosto8),
+			(Gosto2 = Gosto5; Gosto2 = Gosto6; Gosto2 = Gosto7; Gosto2 = Gosto8),
+			(Gosto3 = Gosto5; Gosto3 = Gosto6; Gosto3 = Gosto7; Gosto3 = Gosto8),
+			(Gosto4 = Gosto5; Gosto4 = Gosto6; Gosto4 = Gosto7; Gosto4 = Gosto8).
+
+
+
+/*Regra geral para criar um casal*/
+
+casal(NomeX,NomeY):-pessoa(NomeX,SexoX,OpcaoSexual,IdadeX,ProfissaoX,CidadeX,GeneroFilmeX,EstiloMusicalX,Gosto1, Gosto2, Gosto3, Gosto4)
+	            ,pessoa(NomeY,SexoY,OpcaoSexual,IdadeY,ProfissaoY,CidadeX,GeneroFilmeY,EstiloMusicalY,Gosto1, Gosto2, Gosto3, Gosto4)
+		    ,NomeX \= NomeY, 
+		    ifThenElse(OpcaoSexual = homo, SexoX = SexoY, SexoX\=SexoY), 
+		    idade(IdadeX, IdadeY),
+		    gostos(NomeX, NomeY).
+
 
 
 
